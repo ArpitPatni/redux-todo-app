5aff864f13f996599301f0e637c504309e801ae2
ArpitPatni <arpitpatni00@gmail.com>
2022-07-24 00:00:16 +0530

    feat:Made TodoApp using redux

--- src/App.js
+++ src/App.js
@@ -1,22 +1,64 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { AddTodoAction, RemoveTodoAction } from './actions/TodoActions';
 import './App.css';
 
 function App() {
+  const [todo, setTodo] = useState();
+  const dispatch = useDispatch()
+  const Todo = useSelector((state) => state.Todo)
+  const { todos } = Todo;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(AddTodoAction(todo))
+  }
+
+  const removeHandler=(t)=>{
+    dispatch(RemoveTodoAction(t))
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Todo List App in Redux</h1>
+        <form onSubmit={handleSubmit}>
+          <input placeholder='Enter a Todo'
+            style={{
+              width: 350,
+              padding: 10,
+              borderRadius: 20,
+              border: "none",
+              fontSize: 20
+            }}
+            onChange={(e) => setTodo(e.target.value)}
+          />
+          <button type='submit'
+            style={{
+              padding: 12,
+              borderRadius: 25,
+              fontSize: 15,
+              marginLeft: 20,
+
+            }}
+          >
+            Go</button>
+        </form>
+        <ul className='allTodos'>
+          {todos && todos.map((t) => (
+            <li key={t.id} className='singleTodo'>
+              <span className='todoText'>{t.todo}</span>
+              <button style={{
+                borderRadius: 20,
+                padding: 10,
+                marginLeft: 25
+              }} 
+              onClick={()=>removeHandler(t)}
+              >X</button>
+            </li>
+          ))}
+
+        </ul>
       </header>
     </div>
   );
